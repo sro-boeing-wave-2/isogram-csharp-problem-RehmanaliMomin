69d50e89eba64d50186700e4bfa0015755bc7678
rehman <rehmanmomin@gmail.com>
2018-07-21 14:06:23 +0530

    final commit

--- Isogram.Tests/UnitTest1.cs
+++ Isogram.Tests/UnitTest1.cs
@@ -6,9 +6,45 @@ namespace Isogram.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Sentence_empty()
         {
+            Isogram.IsIsogram("").Equals(true);
+        }
 
+        [Fact]
+        public void Sentence1()
+        {
+            Isogram.IsIsogram("qwertyuiop").Equals(true);
+        }
+
+        [Fact]
+        public void Sentence2()
+        {
+            Isogram.IsIsogram("qwerty asdfg werty dfg").Equals(false);
+        }
+
+        [Fact]
+        public void Sentence3()
+        {
+            Isogram.IsIsogram("qwertyuiop asdfghjkl zxcvbnm,").Equals(true);
+        }
+
+        [Fact]
+        public void Sentence4()
+        {
+            Isogram.IsIsogram("heyohkklkl").Equals(false);
+        }
+
+        [Fact]
+        public void Sentence5()
+        {
+            Isogram.IsIsogram("dfghj-cvbn-poiuy").Equals(true);
+        }
+
+        [Fact]
+        public void Sentence6()
+        {
+            Isogram.IsIsogram("Sharath tatti hain").Equals(false);
         }
     }
 }
--- Isogram/Class1.cs
+++ Isogram/Class1.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Isogram
 {
-    public class Class1
+    public class Isogram
     {
+
+        static HashSet<char> set = new HashSet<char>();
+
+        public static bool IsIsogram(string s)
+        {
+            char[] arr = s.ToCharArray();
+
+            foreach (char item in arr)
+            {
+                if (set.Contains(item) && Regex.IsMatch(item.ToString(), @"^[a-zA-Z]+$")) return false;
+                set.Add(item);
+            }
+            return true;
+        }
+
     }
 }
